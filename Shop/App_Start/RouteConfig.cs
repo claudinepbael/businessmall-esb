be6020614b484be312f3cffd471d67e36e026d8b
jronatay <jronatay25@gmail.com>
2017-01-20 15:30:21 +0800

    businessmall.application structure

--- Shop/App_Start/RouteConfig.cs
+++ Shop/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Shop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Security", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Shop/Controllers/SecurityController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Businessmall.Application.Queries.ShopUser;
+using Businessmall.Application.QueryHandlers;
+
+namespace Shop.Controllers
+{
+    public class SecurityController : Controller
+    {
+        //
+        // GET: /Security/
+
+        public ActionResult Index()
+        {
+            return View(new UserLogin());
+        }
+
+
+    }
+}
