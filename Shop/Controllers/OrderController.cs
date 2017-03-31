ac4cb246856c0eed1698bce0b5c50b9dca9aa786
Claudine Bael <Bael@greenova.de>
2017-03-31 16:02:01 +0800

    Added commands and events for ordering

---
+++ Shop/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Businessmall.Application.Infrastracture.Contracts;
+using Businessmall.Application.Commands;
+
+namespace Shop.Controllers
+{
+    public class OrderController : BaseController
+    {
+        //
+        // GET: /Order/
+        public OrderController (IQueryDispatcher queryDispatcher,ICommandDispatcher commandDispatcher) :base (queryDispatcher,commandDispatcher){
+
+        }
+
+        public ActionResult Index(int orderId)
+        {
+            return View();
+        }
+
+        public ActionResult PlaceOrder(PlaceOrderCommand command) 
+        {
+            return View();
+        }
+
+
+    }
+}
