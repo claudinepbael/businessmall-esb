d410319ef8c2b81b278608d92471fe9399e68282
jronatay <jronatay25@gmail.com>
2017-04-25 18:07:05 +0800

    update inventory shop command and handler

---
+++ Businessmall.ServiceBus.Subscriber.Order/CommandHandlers/ShopProductInventoryUpdateCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using Businessmall.SB.Subscriber.Order.Commands;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businessmall.SB.Subscriber.Order.CommandHandlers {
+    public partial class CommandHandler {
+        
+
+
+        public bool UpdateShopProductInventory(ShopProductInventoryUpdateCommand command){
+
+            try{
+                SqlConnection SqlCon = new SqlConnection(Properties.Settings.Default.ShopDBConnection);
+                if (SqlCon.State == System.Data.ConnectionState.Closed)
+                {
+                    SqlCon.Open();
+                }
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = SqlCon;
+                sqlCommand.CommandText = UpdateSqlQuery();
+                sqlCommand.Parameters.Add(new SqlParameter("@productID", SqlDbType.Int) { Value = command.productID });
+               
+
+
+                int numberOfRowsAffected = sqlCommand.ExecuteNonQuery();
+
+                return true;
+            }catch(Exception e ){
+                return false;
+            }
+            
+            
+
+        }
+
+        private string UpdateSqlQuery (){
+            return @"
+                
+                    Update S set S.available_qty= bo.available_qty FROM [esb.shop].dbo.Product S
+                    INNER JOIN(
+	                    SELECT id, available_qty 
+	                    FROM [esb.businessmall].dbo.Product 
+	                    WHERE id = @productID
+                    ) as bo ON bo.id  = S.product_id
+                    WHERE S.product_id = @productID
+
+
+            ";
+        }
+
+    }
+}
---
+++ Businessmall.ServiceBus.Subscriber.Order/Commands/ShopProductInventoryUpdateCommand.cs
@@ -0,0 +1,12 @@
+﻿using Businessmall.Application.Infrastracture.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businessmall.SB.Subscriber.Order.Commands {
+    public class ShopProductInventoryUpdateCommand : ICommand {
+        public int productID {get;set;}
+    }
+}
